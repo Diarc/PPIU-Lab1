ecca3279dc94551ed2b218be2797dd3f63d802ef
KoksMalina <furio@poczta.onet.eu>
2020-04-06 20:14:30 +0200

    Student 2

---
+++ src/Student2.java
@@ -0,0 +1,5 @@
+
+public class Student2 {
+	String author= "Micha³ Siuda";
+
+}
--- src/showVersion.java
+++ src/showVersion.java
@@ -1,4 +1,10 @@
-
-public class showVersion {
-
+import java.io.Console;
+public class showVersion{
+	public static void main (String [] args)
+	{
+		String ver=System.getProperty("java.version");
+		String vendor=System.getProperty("java.vendor");
+		System.out.println(ver);
+		System.out.println(vendor);
+	}
 }
