1962c82b22de41ce56e0ebffa073b4ae99b35bbe
Di Arc <kamil62261@wp.pl>
2020-04-06 20:03:39 +0200

    Dodano nazwę projektu do klasy głównej

--- src/showVersion.java
+++ src/showVersion.java
@@ -1,4 +1,11 @@
-
+import java.io.Console;
 public class showVersion {
 
+	public static void main(String[] args) {
+		String version = System.getProperty("java.version");
+		String vendor = System.getProperty("java.vendor");
+		System.out.println(version);
+		System.out.println(vendor);
+		System.out.println("PPIU-Lab1");
+	}
 }
